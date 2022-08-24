d4c5404db31cf36f15bcc6d39831513fb3bb188b
sbabureddy <sbabureddy01@gmail.com>
2022-08-24 17:40:13 +0530

    Fix carousel touch icon size

--- client/src/components/Carousel/index.js
+++ client/src/components/Carousel/index.js
@@ -1,11 +1,17 @@
 import React, { memo } from "react";
-import { Paper } from "@mui/material";
+import Paper from "@mui/material/Paper";
 import Carousel from "react-material-ui-carousel";
+import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
 
 const Banner = ({ resource }) => {
   const carouselData = resource.banners.read();
   return (
-    <Carousel>
+    <Carousel
+      className="carousel"
+      IndicatorIcon={
+        <FiberManualRecordIcon size="large" sx={{ fontSize: "48px" }} />
+      }
+    >
       {carouselData?.map(({ id, bannerImageAlt, bannerImageUrl }, ind) => (
         <Paper key={id} sx={{ height: 304 }}>
           <img
